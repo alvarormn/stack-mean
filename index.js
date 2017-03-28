b4e0b4cc428e39d294aef462efabab7cfb32972c
Alvaro <arodriguezm@urbaser.com>
2017-03-28 16:55:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
+
+
+mongoose.connect('mongodb://localhost:27017/stack-mean', (err, res) =>{
+  if (err) {
+    throw err;
+  }else {
+    console.log("La conexión BBDD esta correctamente");
+    app.listen(port, function() {
+      console.log("Servidor escuchando en http://localhost:" + port);
+    })
+  }
+});
