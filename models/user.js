35a33819313a3957f17a746e032c444c9daa379b
Alvaro <arodriguezm@urbaser.com>
2017-03-28 18:32:05 +0200

    ad

---
+++ models/user.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var mongoose = require(mongoose);
+var Schema = mongoose.Schema;
+
+var userSchema = Schema({
+  name: String,
+  nodo:
+});
+
+module.exports = mongoose.model('User', userSchema);
